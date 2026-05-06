c9d23a181dd17f0fe8468fd3d0b626b47b12fc57
GusCrevelari <gustavocrevelari@hotmail.com>
2026-05-06 19:53:06 -0300

    chore: initialize Spring Boot project

---
+++ src/test/java/br/com/fiap/arkive/ArkiveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.arkive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
